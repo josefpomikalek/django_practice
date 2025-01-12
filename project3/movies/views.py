4947129f6f971713974513f3d2d0819cc8224119
josefpomikalek <josef_pomikalek@yahoo.com>
2025-01-12 20:11:27 +0100

    Create movies_list.

--- project3/movies/views.py
+++ project3/movies/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 
 # Create your views here.
+movies_list = {
+    'harrypotter': 'Harry Potter je čaroděj.',
+    'mortalcombat': 'Mortal Combat je zvláštní film.',
+    'smrtvdesti': 'Smrt v dešti je arabská detektivka',
+}
+
+
