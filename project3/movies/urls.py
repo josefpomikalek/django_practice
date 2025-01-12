4ebac0e093e5be7aa0cb2f9569e67f8d772add78
josefpomikalek <josef_pomikalek@yahoo.com>
2025-01-12 20:02:05 +0100

    Set up path in both urls.py.

--- project3/movies/urls.py
+++ project3/movies/urls.py
@@ -1,3 +1,8 @@
 # URLS v aplikaci
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('<int:movienumber', views.allmovies_number),
+    path('<str:moviename>', views.allmovies_text),
+]
