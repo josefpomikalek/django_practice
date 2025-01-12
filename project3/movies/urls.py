18d6805ac52d9215f632e8293e5708503b57fc46
josefpomikalek <josef_pomikalek@yahoo.com>
2025-01-12 21:05:30 +0100

    Create the function allmovies_number().

--- project3/movies/urls.py
+++ project3/movies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:movienumber', views.allmovies_number),
-    path('<str:moviename>', views.allmovies_text),
+    path('<int:movienumber>', views.allmovies_number),
+    path('<str:moviename>', views.allmovies_text, name = 'movie_url'),
 ]
--- project3/movies/views.py
+++ project3/movies/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
 movies_list = {
     'harrypotter': 'Harry Potter je čaroděj.',
     'mortalcombat': 'Mortal Combat je zvláštní film.',
-    'smrtvdesti': 'Smrt v dešti je arabská detektivka',
+    'smrtvdesti': 'Smrt v dešti je arabská detektivka.',
 }
 
 def allmovies_text(request, moviename):
@@ -14,5 +14,15 @@ def allmovies_text(request, moviename):
         movie_info = movies_list[moviename]
         return HttpResponse(movie_info)
     except:
-        return HttpResponseNotFound('Film nebyl nalezen.')
+        return HttpResponseNotFound('Film nenalezen.')
+
+
+def allmovies_number(request, movienumber):    
+    movies_names_list = list(movies_list.keys())
     
+    if movienumber > len(movies_names_list):
+        return HttpResponseNotFound('Film nebyl nalezen.')
+
+    current_movie = movies_names_list[movienumber - 1]
+    redirect_url = reverse('movie_url', args=[current_movie])
+    return HttpResponseRedirect(redirect_url)
