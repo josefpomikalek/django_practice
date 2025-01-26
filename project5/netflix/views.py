ce19e7b1e36695080c9e70550e572f19515d8288
josefpomikalek <josef_pomikalek@yahoo.com>
2025-01-26 15:00:52 +0100

    Render the files index.html and all_movies.html in views.py.

--- project5/netflix/views.py
+++ project5/netflix/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def starting_page(request):
-    pass
+    return render(request, 'netflix/index.html')
 
 def all_movies_page(request):
-    pass
+    return render(request, 'netflix/all_movies.html')
 
