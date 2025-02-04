9e7e4ea056d81b9c63e7a36d2e5390248a9b1d4d
josefpomikalek <josef_pomikalek@yahoo.com>
2025-02-04 21:01:31 +0100

    Import 'path' and 'views' in urls.py in app folder and det up urlpatterns.

--- project6/movies/urls.py
+++ project6/movies/urls.py
@@ -1,1 +1,9 @@
 # URLS v aplikační složce
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.start, name='starting-page'),
+    path('all-movies', views.all_movies, name='all-movies-page'),
+    path('all-movies/<slug:slug>', views.movie_detail, name='movie_detail-page'),
+]
